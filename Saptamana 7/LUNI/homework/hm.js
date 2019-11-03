fe8a2660afd36c8a2d2f9bd4970bab04d374aaab
Dirzu Nicu <nicu.dirzu@gmail.com>
2019-11-03 19:20:46 +0200

    update

--- Saptamana 7/LUNI/homework/hm.js
+++ Saptamana 7/LUNI/homework/hm.js
@@ -73,3 +73,28 @@ tablaInmultirii(1);
 poate obtine pentru punctajele incepand de 1 la 10. (exemplu: “Pentru 2, obtii calificativul E. \n Pentru 7, obtii
 calificativul B \n ...samd “). Atentie: Rezultatul calificativului trebuie sa fie obtinut prin utilizarea functiei
 dezvoltate la tema anterioara !  */
+
+var calificativ = function(punctaj){
+    // prin metoda switch verificam daca punctajul se incadreaza in normele definite
+    switch(true){
+        //cazul 1 comparata daca numarul introduce este mai mic sau egal 1 sau mai mic decat 3, daca rezultatul este corect functia se va opri si  se va afisa textul din "return ....", daca nu va compara mai departe...
+        case (punctaj <= 1 || punctaj < 3):
+            return "Calificatul corespunzator punctajului " + punctaj + " este " + " E";
+            break;
+        case (punctaj <= 3 || punctaj <= 6):
+            return "Calificatul corespunzator punctajului " + punctaj + " este " + " D";;
+            break;
+        case (punctaj <= 7 || punctaj <= 8):
+            return "Calificatul corespunzator punctajului " + punctaj + " este " + " B";;
+            break;
+        case (punctaj === 9):
+            return "Calificatul corespunzator punctajului " + punctaj + " este " + " A";;
+            break;
+        case (punctaj === 10):
+            return "Calificatul corespunzator punctajului " + punctaj + " este " + " A+";
+            break;
+            // in cazul in care paramentrul introdus nu corespunde cu cazurile de mai sus, se va afisa un text standard care il definim ca "default".
+        default:
+            return "Introdu punctajul de la 1 la 10";
+    }
+}
