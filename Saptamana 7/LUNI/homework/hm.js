a181d717d9aef12dde9151fc379db86e19fe173a
Dirzu Nicu <nicu.dirzu@gmail.com>
2019-11-03 21:07:41 +0200

    final homework luni

--- Saptamana 7/LUNI/homework/hm.js
+++ Saptamana 7/LUNI/homework/hm.js
@@ -5,7 +5,18 @@ Scrieti functia in doua variante: while si for
 */
 
 /*
--
+-----------v1----------------
+-Folosim "Named Functions"
+- folosesc metoda "for" unde 1.declaram variabila i = 0, 2. nr de loop-uri sa fie  20, 3. la fiecare bucla incrementam i++.
+- folosim metoda if/else si modulo ca sa afla numarul par, iar in caz contrat e impar.
+-rezultatul obtinut va fi i este un numar par/impar
+-----------v2----------------
+v2
+- declaram variabila i = 0
+- folosesc metoda "while" unde i<= 20, 3. 
+- folosim metoda if/else si modulo ca sa afla numarul par, iar in caz contrat e impar.
+-rezultatul obtinut va fi i este un numar par/impar
+- la urmatoare repetare i++ ( i + 1) 
 */
 function verificaNumar (){
 /*v1
@@ -18,6 +29,8 @@ function verificaNumar (){
     }
 }
 v1 */
+
+//v2
 var i = 0;
 while(i <= 20){
     if (i % 2 == 0){
@@ -39,7 +52,17 @@ while(i <= 20){
 rezultatul. (exemplu: “3*9=27”).
 Scrieti functia in doua variante: while si for
 */
-
+/*
+-----------v1----------------
+- am folosit  function expression
+- am folosit metoda "for" unde i =0, numarul de iteratie este 10, iar la fiecare repetare i++.
+- rezultatul va fi i * 9 = rezultat;
+-----------v2----------------
+-am declarat variabila i = 0
+- am folosit metoda "while" cu numarul de iteratii pana la 10
+- rezultatul va fi i * 9 = rezultat;
+- la urmatoare repetare i+1
+*/
 (function(){
     /* v1
     for (var i = 0; i <= 10; i++){
@@ -65,6 +88,15 @@ Scrieti functia in doua variante: while si for
  1 * 10 = 10 )
 Pentru a afisa in consola pe linie noua puteti concatena un string, la final, cu + “\n”.
 */
+
+
+/*
+- Folosim "Named Functions" 
+- folosind metoda "for" , variabila i = 1, numarul de interatii 10, la fiecare iteratie i+1
+- folosim "for" in "for" ca creem un bucla in alta bucla - scopul fiind afisarea fiecarui numar de la 1-10 inmultit cu fieare numar de la 1-10.
+- rezultat "1*1= rezultat";
+*/
+
 function tablaInmultirii(){
     for(var i = 1; i <= 10; i++){
         for (numar = 1; numar <= 10; numar++)
@@ -78,6 +110,15 @@ tablaInmultirii();
 poate obtine pentru punctajele incepand de 1 la 10. (exemplu: “Pentru 2, obtii calificativul E. \n Pentru 7, obtii
 calificativul B \n ...samd “). Atentie: Rezultatul calificativului trebuie sa fie obtinut prin utilizarea functiei
 dezvoltate la tema anterioara !  */
+
+/*
+- folosim funcatia de tip "Anonymous function" de la exercitul anterior care ne afiseaza calificativul in functie de punctaj
+- cream o noua functie de tip "Named Function" in care folosim metoda "for", acesta functia foloseste functia anonima ca sa compare fiecare punctaj si ce calitativ obtinem
+- declaram varibila i = 0, numarul de iteratii - 10, i++
+- rezultatul obtinut va fi de la 1-10  + calitativul corespunzator.
+
+*/
+
 
 var calificativ = function(punctaj){
     // prin metoda switch verificam daca punctajul se incadreaza in normele definite
@@ -103,6 +144,8 @@ var calificativ = function(punctaj){
             return "Introdu punctajul de la 1 la 10";
     }
 }
+
+
 
 function checkCalificativ(){
     for ( var i = 0;i <= 10; i++){
