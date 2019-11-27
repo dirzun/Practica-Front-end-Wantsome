bcbd22254f935de67d742cf220ed9a6e30f78ae5
Dirzu Nicu <nicu.dirzu@gmail.com>
2019-11-27 15:54:33 +0200

    update hm js event

--- Saptamana 10/luni/regForm/newtry/index.js
+++ Saptamana 10/luni/regForm/newtry/index.js
@@ -1,35 +1,33 @@
-const btn = document.getElementById('button');
-const form = document.getElementById('form');
-//error
-let divError = document.createElement('div');
-divError.id="error";
-let textEror =  document.createElement('p');
-let textdiv = divError.appendChild(textEror);
-   
-//username
-const divUser = document.getElementById('username');
-const username =  document.querySelector('input[name = user]').value;
-
-//email
-const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-const divEmail = document.getElementById('email');
-const email =  document.querySelector('input[name = email]').value;
-
-//first&last name
-const divFName = document.getElementById('firstName');
-const firstName = document.querySelector('input[name = fname]').value;
-const divLName = document.getElementById('lastName');
-const lastName = document.querySelector('input[name = lname]').value;
-
-//phone
-const phone = document.querySelector('input[name = phone]').value;
-const divPhone = document.getElementById('phone');
-
-
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-
+    const btn = document.getElementById('button');
+    const form = document.getElementById('form');
+    //error
+    let divError = document.createElement('div');
+    divError.id="error";
+    let textEror =  document.createElement('p');
+    let textdiv = divError.appendChild(textEror);
+       
+    //username
+    const divUser = document.getElementById('username');
+    const username =  document.querySelector('input[name = user]').value;
+    
+    //email
+    const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    const divEmail = document.getElementById('email');
+    const email =  document.querySelector('input[name = email]').value;
+    
+    //first&last name
+    const divFName = document.getElementById('firstName');
+    const firstName = document.querySelector('input[name = fname]').value;
+    const divLName = document.getElementById('lastName');
+    const lastName = document.querySelector('input[name = lname]').value;
+    
+    //phone
+    const phone = document.querySelector('input[name = phone]').value;
+    const divPhone = document.getElementById('phone');
+    
     if (username.length < 5 || username === " "){
         e.preventDefault();
         divUser.appendChild(divError).appendChild(textdiv).textContent = `Please enter a valid username`;
@@ -50,4 +48,4 @@ form.addEventListener('submit',(e)=>{
         e.preventDefault();
         divPhone.appendChild(divError).appendChild(textdiv).textContent = `Must be at least 10 characters`;
     }
-},true);
+},false);
